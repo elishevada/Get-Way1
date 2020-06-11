2783e188f856ac7092accfa8d0c616ee236772d3
rivkagozlan <r4gozlan@gmail.com>
2020-06-11 11:20:46 +0300

    The project classes, Vec and client are only to checking

---
+++ Vec.py
@@ -0,0 +1,18 @@
+class Vec:
+    def __init__(self):
+        self.__vec = []
+        
+    def getVec(self):
+        return self.__vec
+    
+    #The function clear a vector
+    def clearVec(self):
+        self.__vec.clear()
+    
+    #The function concatenate a vector to the class's vector. 
+    def concatVecs(self, secVec):
+        self.__vec.extend(secVec)
+        
+    #The function appends list with an item or adds an item at the end of the list.
+    def vecAppend(self, index, val):
+        self.__vec.append([index,val])        
